9f352c521544bf21d9265302f937685960e7714b
sumitha <129954748+sumithavenkataswamy@users.noreply.github.com>
2025-04-06 18:17:05 +0530

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace LearningGithubContainerRegistry.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "added"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
